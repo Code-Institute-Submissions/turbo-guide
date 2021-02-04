7941cf01f2da9308c10df59c98ac4572f3678900
madatoo <magda.ruszaj@gmail.com>
2021-02-04 15:32:56 +0000

    new map feature  added to map in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -112,5 +112,65 @@ function initMap() {
     const sligoTownMarker = new google.maps.Marker({
     position: sligo,
     map,
-    })
+    });  
+
+    // Create markers.
+  
+    for (let i = 0; i < locations.length; i++) {
+     const pins = new google.maps.Marker({
+     position: locations[i].position,
+     icon: icons[markerArray[i].type].icon,
+     map: map,
+});
+
+  }
+// add markers
+  function addMarker(feature) {
+  var pins = new google.maps.Marker({
+    position: locations.position,
+    icon: icons[markersArray.type].icon,
+    map: map
+  });
+
+  pins.push(marker);
+}
+// // This example displays a marker at the center of Australia.
+// When the user clicks the marker, an info window opens.
+
+const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
+    '<div id="bodyContent">' +
+    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
+    "sandstone rock formation in the southern part of the " +
+    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
+    "south west of the nearest large town, Alice Springs; 450&#160;km " +
+    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
+    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
+    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
+    "Aboriginal people of the area. It has many springs, waterholes, " +
+    "rock caves and ancient paintings. Uluru is listed as a World " +
+    "Heritage Site.</p>" +
+    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+    "https://en.wikipedia.org/w/index.php?title=Uluru</a> "</p>" +
+    "</div>" +
+    "</div>";
+  
+    const infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map,
+    title: "sligo (Ayers Rock)",
+  });
+  marker.addListener("click", () => {
+    infowindow.open(map, marker);
+  });
+}
+
+
 }
