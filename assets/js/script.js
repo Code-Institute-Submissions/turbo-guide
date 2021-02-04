72eedc020e6bfb538ee4bade77a8679bdb9e4ae9
madatoo <magda.ruszaj@gmail.com>
2021-02-04 20:01:55 +0000

    set map funcions to add markers and rebuild both arrays wirh markers amd places

--- assets/js/script.js
+++ assets/js/script.js
@@ -123,9 +123,8 @@ function initMap() {
      map: map,
 });
 
-  }
 // add markers
-  function addMarker(feature) {
+  function addMarker(location) {
   var pins = new google.maps.Marker({
     position: locations.position,
     icon: icons[markersArray.type].icon,
@@ -134,43 +133,10 @@ function initMap() {
 
   pins.push(marker);
 }
-// // This example displays a marker at the center of Australia.
-// When the user clicks the marker, an info window opens.
 
-const contentString =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>' +
-    '<div id="bodyContent">' +
-    "<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large " +
-    "sandstone rock formation in the southern part of the " +
-    "Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) " +
-    "south west of the nearest large town, Alice Springs; 450&#160;km " +
-    "(280&#160;mi) by road. Kata Tjuta and Uluru are the two major " +
-    "features of the Uluru - Kata Tjuta National Park. Uluru is " +
-    "sacred to the Pitjantjatjara and Yankunytjatjara, the " +
-    "Aboriginal people of the area. It has many springs, waterholes, " +
-    "rock caves and ancient paintings. Uluru is listed as a World " +
-    "Heritage Site.</p>" +
-    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
-    "https://en.wikipedia.org/w/index.php?title=Uluru</a> "</p>" +
-    "</div>" +
-    "</div>";
-  
-    const infowindow = new google.maps.InfoWindow({
+// setup window for some informations about place
+const infoWindow = new google.maps.infoWindow({
     content: contentString,
-  });
-
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map,
-    title: "sligo (Ayers Rock)",
-  });
-  marker.addListener("click", () => {
-    infowindow.open(map, marker);
-  });
+    maxwidth:400
 }
-
-
-}
+)}}
