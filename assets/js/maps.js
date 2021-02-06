fbe8d1fc18ac9ba9e0534ad877b2704fef0ed84d
madatoo <magda.ruszaj@gmail.com>
2021-02-06 12:19:48 +0000

    another js validation with codebeautify.org/jsvalidate

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,13 +1,19 @@
-var infoObj = [];
+const sligo = {
+        lat: 54.2697,
+        lng: -8.4694
+    },
 
-//const sligo = {
-       // lat: 54.2697,
-       // lng: -8.4694
-   // },
+function sligoMap() {  
+    let map = new google.maps.Map(document.getElementById("map"), {
+    center: sligo,
+    zoom: 8
+    });
 
-    //individual markers 
-    //I took an idea to create this array in that way from ryanjbm and customise it for my project. I needed customise it much more with tutorial:  https://www.youtube.com/watch?v=Xptz0GQ2DO4&t=356s  Icons are taken form: https://mapicons.mapsmarker.com/
+    addMarkerInfo()
+    
+};
 
+    // array with places   
     const markersArray = [
 
         {
@@ -63,112 +69,97 @@ const locations = [
     // Beaches
     //Rosses Point
     {
-        LatLng: [{
+        coordinates: {
             lat: 54.306250,
             lng: -8.567500,
-        }],
+        },
         type: "shore",
         placeName: "Rosses Point",
-        information: "#"
+        information: "This is one of my favourite places on the coast. I love this old seafaring village that has a seaside resort with a 18-hole championship golf course. If you like sailing you can hire a fully equipped boat and enjoy your day at the sea."
     },
 
     //Strandhill
     {
-        LatLng: [{
+        coordinates:{
             lat: 54.306250,
             lng: -8.567500,
-        }],
+        },
         type: "surfing",
         placeName: "Strandhill",
-        information: "#"
+        information: "This is location has been named as one of the best places to surf in Europe. It is worth noting that swimming is forbidden at Strandhill beach due to very strong sea currents."
     },
 
     // Town attractions
 
     //Sligo Town
     {
-        LatLng: [{
+        coordinates: {
             lat: 54.2697,
             lng: -8.4694,
-        }],
+        },
         type: "smallcity",
         placeName: "Sligo",
-        information: "#"
+        
     },
 
     //Sligo Abbey
     {
-        LatLng: [{
+        coordinates: {
             lat: 54.27087,
             lng: -8.47002,
-        }],
+        },
         type: "ruins",
         placeName: "Sligo Abbey",
-        information: "#"
+        information: "This place surprised me very much. It was founded in the mid 13th cenury by Maurice Fitzgerald and is, still, one of the most distinguishable places in Sligo."
     },
 
     //Yeats Building
     {
-        LatLng: [{
+        coordinates: {
             lat: 54.27376,
             lng: -8.47213,
-        }],
+        },
         type: "museum",
         placeName: "Yeats Building",
-        information: "#"
+        information: "There is a permanent exhibition on the life and works of W.B.Yeats and Yeats Art Gallery presents changing exhibitions of art, sculpture, crafts etc."
     },
 
     // Nearby attractions
 
     //Lisadell House and Gardens 
     {
-        LatLng: [{
+        coordinates:{
             lat: 54.34645,
             lng: -8.58279,
-        }],
+        },
         type: "palace",
         placeName: "Lisadell House and Gardens",
-        information: "#"
+        information: "This is an amazing historical place sorrounded by a beautiful large garden. Located in proximity to the majestic table mountain, Benbulben."
     },
 
     //Mullaghmore
     {
-        LatLng: [{
+        coordinates: {
             lat: 54.46555,
             lng: -8.45219,
-        }],
+        },
         type: "restaurant",
         placeName: "Mullaghmore",
-        information: "#"
+        information: "Small fishing village with a superb, large (3km) sandy beach. Mullaghmore is also a base for licensed angling/passanger boats for charter."
     },
 ];
 
-
-//function initMap() {  
-let map = new google.maps.Map(document.getElementById("map"), {
-    //center: sligo,
-    center: {lat:54.2697,lng:-8.4694},
-    zoom: 8
-});
-
-addMarkerInfo();
+var infoObj = [];
 
 
-
-//display window with map
-window.onload = function() {
-    initMap();
-};
-
-// Create markers.
 function addMarkerInfo() {
     for (var i = 0; i < locations.length; i++) {
-        var contentString = `<h3> ${'locations[i].placeName'}</h3>`;
-        //"<h3>" + "locations[i].placeName" + "</h3>"
+        var contentString = `<h3> ${'locations[i].placeName'}</h3>`+ `<p> ${'locations[i].information'}</p>`;
         const marker = new google.maps.Marker({
-            position: locations[i].LatLng[0],
+            position: locations[i].coordinates[0],
             icon: icons[markersArray[i].type].icon,
-            map: map
+            map: map,
+            animation: google.maps.Animation.DROP
         });
 
         const infowindow = new google.maps.InfoWindow({
@@ -190,16 +181,7 @@ function addMarkerInfo() {
             infoObj[0].length = 0;
         }
     }
-
-    //till here it looks ok 
-    //function initMap() {
-    // map = new google.maps.Map(document.getElementById("map"), {
-    //  center: sligo,
-    //  zoom: 8
-    //  });
-
-    // addMarkerInfo();
-
-}
+};
+    
 /* call the map*/
 myMap();
